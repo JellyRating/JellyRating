2b83ae2e9549af1e8fc818d1f647b0b4cb406a14
Felipe Brigalante <felipe.brigalante@gmail.com>
2015-10-04 02:20:29 -0300

    login funcionando

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,23 @@
 class SessionsController < ApplicationController
-  def new
+  def new    
+      redirect_to user_path current_user if logged_in?
   end
 
   def create
-  	user = User.find_by(email: params[:session][:email].downcase)
-  	if user and user.authenticate(params[:session][:password])
-  		flash[:notice] = "Welcome back #{user.name}."
-  		log_in user
-  		redirect_to user
-  	else
-  		flash[:warning] = ['Invalid email/password combination']
-  		render 'new'
-  	end
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user and user.authenticate(params[:session][:password])
+      flash[:notice] = "Welcome back #{user.name}."
+      log_in user
+      params[:remember_me] == "1" ? remember(user) : forget(user)
+      redirect_to user
+    else
+      flash[:warning] = ['Invalid email/password combination']
+      render 'new'
+    end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to login_path
   end
 end
---
+++ spec/factories/user.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Naruto Uzumaki"
+    email "naruto@shippuden.com"
+    password ""
+    password_confirmation ""
+  end
+end
