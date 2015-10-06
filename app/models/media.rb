caea2ca870807be0eb68e5dc52b13d435b9cabe6
saasbook <saasbook@saasbook.(none)>
2015-10-06 10:05:18 -0300

    MovieAvaliation Model and migration

--- app/models/media.rb
+++ app/models/media.rb
@@ -2,4 +2,6 @@ class Media < ActiveRecord::Base
     before_save {self.title = title.downcase};
     validates :title, presence: true;
     validates :release_date, presence: true;
+
+    has_many :media_avaliations
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
             uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
-  
+  has_many :media_avaliations
 
   # Returns the hash digest of the given string.
   def User.digest(string)
