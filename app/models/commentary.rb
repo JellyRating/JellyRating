1ff1d8eb43f1537b9f8ae8a8e75f277aed32e794
mateus <mmrocha94@gmail.com>
2015-11-07 17:42:31 -0200

    Last commentaries first

--- app/models/commentary.rb
+++ app/models/commentary.rb
@@ -2,4 +2,8 @@ class Commentary < ActiveRecord::Base
   belongs_to :user 
   belongs_to :commentable, :polymorphic => true
   
+
+  def self.last_commentaries
+    order('created_at desc')
+  end
 end
