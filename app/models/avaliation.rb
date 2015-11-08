f65db88c558056a280d88238f358f70b8d4681e9
Gian <gian_l.cicca@hotmail.com>
2015-11-08 17:05:27 -0400

    Listed avaliations in user page

--- app/models/avaliation.rb
+++ app/models/avaliation.rb
@@ -3,4 +3,12 @@ class Avaliation < ActiveRecord::Base
 	belongs_to :rateable, :polymorphic => true
 	validates_inclusion_of :rating, in: [true, false]
 	validates :user_id, :uniqueness => { :scope => [:rateable_type, :rateable_id] }
+
+	def aval_str
+		if self.rating
+			'Liked'
+		else
+			'Disliked'
+		end
+	end
 end
