5cb7b9967e077e2bd5d5707af6aaab0044e29719
Gian <gian_l.cicca@hotmail.com>
2015-12-03 17:22:13 -0400

    Homepage now available for logged off users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  skip_before_filter :set_current_user, only: [:index]
   def index
   	#@recent_items = Item.find(:all, :order => "created_at desc", :limit => 5).reverse
   	@recent_items = Item.order("created_at desc").take(5)
